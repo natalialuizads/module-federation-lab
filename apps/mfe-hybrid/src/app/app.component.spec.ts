87291e26fb4380681f8b19144ef09e2d14f7df39
Natália Luiza <natalialuizads@gmail.com>
2024-05-29 13:33:42 -0300

    include template mfe hiybrid

--- apps/mfe-hybrid/src/app/app.component.spec.ts
+++ apps/mfe-hybrid/src/app/app.component.spec.ts
@@ -15,13 +15,13 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome mfe-web-components'
+      'Welcome mfe-hybrid'
     );
   });
 
-  it(`should have as title 'mfe-web-components'`, () => {
+  it(`should have as title 'mfe-hybrid'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mfe-web-components');
+    expect(app.title).toEqual('mfe-hybrid');
   });
 });
