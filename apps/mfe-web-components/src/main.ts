8e57dab3fd70a9daa3b2929c8360b45c97306c6b
Natália Luiza <natalialuizads@gmail.com>
2024-05-29 14:49:59 -0300

    configure web component

--- apps/mfe-web-components/src/main.ts
+++ apps/mfe-web-components/src/main.ts
@@ -1,7 +1,13 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { createApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { createCustomElement } from '@angular/elements';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+(async () => {
+  const app = createApplication(appConfig);
+
+  const customCardElement = createCustomElement(AppComponent, {
+    injector: (await app).injector
+  });
+  customElements.define('mfe-web-components', customCardElement);
+})();
